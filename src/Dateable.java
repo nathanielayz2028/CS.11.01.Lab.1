baa96e2ad0b7408a7c5567b594330b8e140c9509
nathanielayz2028 <nathanielayz2028@student.cis.edu.hk>
2025-09-10 16:55:50 +0800

    hello

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        int age;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your age: ");
+        age = scanner.nextInt();
+        System.out.print(age + "-year olds should date somebody who is at least " + (7+age/2) + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        int age;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter your age: ");
+        age = scanner.nextInt();
+        System.out.print(age + "-year olds should read at least " + (100 - age) + " pages before giving up.");
 
     }
 }
